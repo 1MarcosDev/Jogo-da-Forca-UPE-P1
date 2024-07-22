81b8adc748971e522265d12e25a9cefd56bc3c33
Pedro Delgado <166747454+PedroViniciusMD@users.noreply.github.com>
2024-07-22 16:50:17 -0300

    Add files via upload
    função menu

---
+++ menu.py
@@ -0,0 +1,23 @@
+import time
+
+def menu ():
+    print("")
+    print("-----------------MENU--------------------")
+    time.sleep(1)
+    print("BEM VINDO AO JOGO DA FORCA !!")
+    print("")
+    time.sleep(1)
+    print("Escolha uma opção abaixo:")
+    time.sleep(1)
+    print("1 - Jogar")
+    time.sleep(1)
+    print("2 - Instruções")
+    time.sleep(1)
+    print("3 - Sair")
+    time.sleep(1)
+    print("4 - Autores")
+    time.sleep(1)
+    print("5 - Ver rank do jogo")
+    print("-----------------------------------------")
+   
+# OBS --- CHAMAR O MENU PRIMEIRO DO QUE CHAMAR A VARIAVEL OPCAO
