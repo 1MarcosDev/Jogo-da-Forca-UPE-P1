2a6894f127e2d83c80d07626a4d93c3b7174718c
Marcos Antônio <90413458+1MarcosDev@users.noreply.github.com>
2024-07-28 16:45:13 +0000

    Instruções implementada no local correto

--- menu.py
+++ menu.py
@@ -9,7 +9,22 @@ def jogar():
     pass
 
 def instrucoes():
-    instrucoesforca()
+    print("""
+    Bem-vindo ao Jogo da Forca!
+
+    Instruções:
+    1. O jogo escolhe uma palavra secreta que você deve adivinhar.
+    2. Você verá uma linha para cada letra da palavra secreta.
+    3. Em cada rodada, você pode tentar adivinhar uma letra da palavra.
+    4. Se a letra estiver na palavra secreta, ela será revelada em todas as posições correspondentes.
+    5. Se a letra não estiver na palavra secreta, você perde uma vida.
+    6. Você tem um número limitado de vidas dependendo da dificuldade que você escolherá. Cada erro custa uma vida.
+    7. O jogo continua até você adivinhar a palavra completa ou perder todas as vidas.
+    8. Se você adivinhar a palavra antes de perder todas as vidas, você ganha!
+    9. Se você perder todas as vidas antes de adivinhar a palavra, você perde.
+
+    Boa sorte e divirta-se jogando!
+    """)
 
 def rank():
     mostrarRecords()
